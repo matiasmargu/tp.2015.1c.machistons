0a907565d95cbb9c5be74472a56893bd5ba48c15
MatiNav <matiasnavarroezequiel@gmail.com>
2015-05-10 01:40:08 -0300

    Aca creo haber hecho el select para nodo del puerto fs

--- Nodo/src/Nodo.c
+++ Nodo/src/Nodo.c
@@ -1,6 +1,6 @@
 /*
  ============================================================================
- Name        : Job.c
+ Name        : Nodo.c
  Author      : 
  Version     :
  Copyright   : Your copyright notice
@@ -15,11 +15,15 @@
 #include <../commons/config.h>
 #include <../commons/log.h>
 #include <../commons/string.h>
+#include <../commons/collections/list.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <socket/socket.h>
 #include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 
 t_log* logger; // Log Global
 
@@ -70,9 +74,81 @@ int main(void) {
 
 	log_info(logger, "Correcta lectura del archivo de configuracion");
 
+////////////////////// Creo El Select ////////////////////////////////////////////////////
+
+	    fd_set master;
+		fd_set read_fds;
+
+		struct sockaddr_in serveraddr;
+		struct sockaddr_in clientaddr;
+
+		int fdmax;
+		int listener;
+
+		int newfd;
+		char buf[1024];
+		int yes = 1;
+		int addrlen;
+		int i;
+		int status_s;
+
+		t_list *listaArchivos;
+
+		pthread_t h1;
+
+		listaArchivos = list_create();
+
+		list_add(listaArchivos, archivo_create("archivo1.txt", 2, "No disponible"));
 
 
+		// que funcion hace el hilo ??
+		// pthread_create(&h1, NULL, ------xxxxxx-----, NULL);
 
+		FD_ZERO(&master);
+		FD_ZERO(&read_fds);
+
+		listener = socket(AF_INET, SOCK_STREAM, 0);
+		setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+
+		serveraddr.sin_family = AF_INET;
+		serveraddr.sin_addr.s_addr = INADDR_ANY;
+		serveraddr.sin_port = htons(puerto_fs);
+		memset(&(serveraddr.sin_zero), '\0', 8);
+		bind(listener, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+		listen(listener, 10);
+		FD_SET(listener, &master);
+
+		fdmax = listener;
+
+		for(;;)
+		{
+		read_fds = master;
+		select(fdmax+1, &read_fds, NULL, NULL, NULL);
+		for(i = 0; i <= fdmax; i++)
+		{
+			if(FD_ISSET(i, &read_fds))
+			{
+				if(i == listener)
+				{
+					addrlen = sizeof(clientaddr);
+					newfd = accept(listener, (struct sockaddr *)&clientaddr, &addrlen);
+					FD_SET(newfd, &master);
+					if(newfd > fdmax)
+					{
+						fdmax = newfd;
+					}
+					// Nueva coneccion
+					// inet_ntoa(clientaddr.sin_addr) -> es la IP de donde se conecto. // creo que es 127.0.0.2
+					// newfd -> es el socket nuevo
+				}
+				else
+				{
+					status_s = recv(i, (void*) buf, PACKAGESIZE, 0);
+					if (status_s != 0) printf("%s", buf);
+				}
+			}
+        }
+   }
 	config_destroy(archivoConfiguracion);
 	log_destroy(logger);
 	free(rutaArchivoConfiguracion);
@@ -81,5 +157,6 @@ int main(void) {
 	free(archivo_bin);
 	free(dir_temp);
 	free(nodo_nuevo);
+	list_destroy(listaArchivos);
 	return EXIT_SUCCESS;
 }
