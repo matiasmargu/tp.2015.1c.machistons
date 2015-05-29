07826f96507081300fbc0dc9c0429b7862d940d7
David George <tdavid2000@hotmail.com>
2015-05-29 17:17:52 -0300

    unos cambios...

---
+++ Marta/src/Funciones.h
@@ -0,0 +1,8 @@
+
+#ifndef FUNCIONES_H_
+#define FUNCIONES_H_
+#include <sys/socket.h>
+#include <socket/socket.h>
+
+
+#endif /* FUNCIONES_H_ */
--- Marta/src/Marta.c
+++ Marta/src/Marta.c
@@ -12,9 +12,9 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <string.h>
-#include <../commons/config.h>
-#include <../commons/log.h>
-#include <../commons/string.h>
+#include <./commons/config.h>
+#include <./commons/log.h>
+#include <./commons/string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
@@ -27,30 +27,25 @@ t_log* logger; // Log Global
 
 int main(void) {
 
+	char* rutaArchivoConfiguracion = "/home/utnso/git/tp-2015-1c-machistons/Configuracion/marta.conf";
+	char* puerto_fs = config_get_string_value(archivoConfiguracion, "PUERTO_FS");
+	char* ip_fs = config_get_string_value(archivoConfiguracion, "IP_FS");
+
 	int entero;
-    char** lista_archivos;
-	struct marta_job Marta_Job;
-	struct job_marta_resultado Job_Marta_Resultado;
+
+		struct job_marta_inicio Job_Marta_Inicio;
+			    struct marta_job Marta_Job;
+			    struct job_marta_resultado Job_Marta_Resultado;
+
 	int socketJob = crearServidor("3000");
+
 	int socketFS = crearCliente("127.0.0.1","3001");
 
 
 
-	if ((recv(socketJob, &lista_archivos, sizeof(lista_archivos),0 )) != 0){// recibo lista nombres de archivos del job
-		send(socketFS,&lista_archivos,sizeof(lista_archivos),0);
-/* aca hay que ver la forma en que el FS nos pasa la lista de bloques
-
-		if ((recv(socketFS, &lista de bloques, sizeof(lista de bloques),0 )) != 0){
-
-			send(socketJob, &lista de bloques, sizeof(lista de bloques),0 ); aca le mando los bloques al job perp
-tambien le tengo que mandar que hacer con que rutina y todo eso, yo creo que habria que hacer una estructura que indique
-la lista de bloques si es mapping o reduce y no se si algo mas...
-
-		}
-*/	}
-
-
-
+	if ((recv(socketJob, &Job_Marta_Inicio, sizeof(struct job_marta_inicio),0 )) != 0){
+		printf("se conecto el Job con la operacion numero %i\n",Job_Marta_Inicio.operacionID);
+	}
 
 	Marta_Job.NumeroBloqueDeDatos = 12;
 	Marta_Job.rutina = "mapper";
