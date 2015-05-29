f7245fc30aefbe4ef1d980b283790733e6f9b4e4
David George <tdavid2000@hotmail.com>
2015-05-29 17:22:16 -0300

    .......

--- Marta/src/Funciones.c
+++ Marta/src/Funciones.c
@@ -3,22 +3,8 @@
 #include <socket/socket.h>
 #include "Marta.c"
 
-struct job_nodo Job_Nodo;
-char* handshake = "conexion establecida";
-struct job_marta_resultado Job_Marta_Resultado;
-struct nodo_job Nodo_Job;
 
 
 
-char* rutaArchivoConfiguracion = "/home/utnso/git/tp-2015-1c-machistons/Configuracion/marta.conf";
-
-
-t_log* logger; // Log Global
-
-
-
-archivoConfiguracion = config_create(rutaArchivoConfiguracion);
-mapper = config_get_string_value(archivoConfiguracion, "MAPPER");
-reduce = config_get_string_value(archivoConfiguracion, "REDUCE");
 
 
