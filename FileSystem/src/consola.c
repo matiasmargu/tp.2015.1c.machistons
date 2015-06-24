2bd35fe7f8dfcde5bfda8f03a4ab066af2967e7b
gcrivelli <gastonghc@gmail.com>
2015-06-23 22:08:52 -0300

    cambios

--- FileSystem/src/consola.c
+++ FileSystem/src/consola.c
@@ -98,6 +98,13 @@ void *atenderConsola(void*arg) {
 }
 
 void imprimirMenu(void){
+	initscr(); //activar el acceso a la pantalla
+	clear(); //limpia la pantalla
+	move(0,30); //mueve el cursos a la posición 0,30
+	printw("Hola");
+	refresh(); //mostrar en pantalla lo escrito por printw
+	getch(); //espera la pulsación de una techa
+	endwin(); //cerrar el acceso a la pantalla
 
 	printf("> Los comandos se ingresan con su numero \n"
 			"> 		COMANDOS \n"
