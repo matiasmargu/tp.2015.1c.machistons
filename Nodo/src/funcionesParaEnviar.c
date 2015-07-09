5616b8b5862f2fbfe5b7e8ba372a04f37fa51f11
nicozar95 <nzarewsky@gmail.com>
2015-07-08 21:48:12 -0300

    Banda de cambios, establecido los scripts y el tema de si un nodo se cae y se recontecta

--- Nodo/src/funcionesParaEnviar.c
+++ Nodo/src/funcionesParaEnviar.c
@@ -52,7 +52,7 @@ int recive_y_deserialisa_CHARp(char *script, int socket, uint32_t tamanioTotal){
 
 
 
-char* serializarIP_PUERTO_ESTADOnodo(char* ip_fs,char* puerto_fs,char* nodo_nuevo, int tamanioData){
+char* serializarIP_PUERTO(char* ip_fs,char* puerto_fs, int tamanioData){
 	int offset = 0;
 	int size_to_send;
 
@@ -76,17 +76,15 @@ char* serializarIP_PUERTO_ESTADOnodo(char* ip_fs,char* puerto_fs,char* nodo_nuev
 	memcpy(serializedPackage + offset, puerto_fs, size_to_send);
 	offset += size_to_send;
 
-	tamanioNombre = strlen(nodo_nuevo) + 1;
+	char* archivo = mapearAMemoriaVirtual(archivo_bin);
+	int tamanioDatos = strlen(archivo);
 	size_to_send = sizeof(int);
-	memcpy(serializedPackage + offset, &tamanioNombre, size_to_send);
-	offset += size_to_send;
-
-	size_to_send =  strlen(nodo_nuevo) + 1;
-	memcpy(serializedPackage + offset, puerto_fs, size_to_send);
-	offset += size_to_send;
+	memcpy(serializedPackage + offset, &tamanioDatos, size_to_send);
+	munmap(archivo,strlen(archivo));
 
 	return serializedPackage;
 }
+
 char* serializarBloqueDeDatos(char* bloque, int tamanioData){
 	int offset = 0;
 	int size_to_send;
@@ -111,44 +109,76 @@ void handshakeConFS (){
 	//Esta es la coneccion con el FS
 	pthread_t hiloFS;
 
-	nuevoArchivo = fopen("/home/utnso/git/tp-2015-1c-machistons/Configuracion/nodo.conf","w");
-
-	fputs("PUERTO_FS=3000\n",nuevoArchivo);
-
-	fputs("IP_FS=",nuevoArchivo);
-	fputs(ip_fs,nuevoArchivo);
-	fputc('\n',nuevoArchivo);
-
-	fputs("ARCHIVO_BIN=",nuevoArchivo);
-	fputs(archivo_bin,nuevoArchivo);
-	fputc('\n',nuevoArchivo);
-
-	fputs("DIR_TEMP=",nuevoArchivo);
-	fputs(dir_temp,nuevoArchivo);
-	fputc('\n',nuevoArchivo);
-
-	fputs("NODO_NUEVO=NO\n",nuevoArchivo);
-	nodo_nuevo = "NO";
-
-	fputs("IP_NODO=",nuevoArchivo);
-	char* ip=obtenerIP();
-	fputs(ip,nuevoArchivo);
-	fputc('\n',nuevoArchivo);
-
-	fputs("PUERTO_NODO=6000\n",nuevoArchivo);
-	fclose(nuevoArchivo);
-
-	leerRutaDeConfiguracion();
-
 	int socket_fs = crearCliente(ip_fs,puerto_fs);
+
 	int entero2 = 2; // handshake con FS
 	char* mensaje;
 
 	send(socket_fs,&entero2,sizeof(int),0);
-	int tamanioData = sizeof(int) + strlen(ip_nodo) + 1 + sizeof(int) + strlen(string_itoa(puerto_nodo)) + 1 + sizeof(int)+strlen(nodo_nuevo)+1;
-	send(socket_fs, &tamanioData, sizeof(int), 0);
-	mensaje = serializarIP_PUERTO_ESTADOnodo(ip_nodo, string_itoa(puerto_nodo),nodo_nuevo, tamanioData);
-	send(socket_fs,mensaje,tamanioData,0);
+
+	if(string_equals_ignore_case(nodo_nuevo,"SI")){
+
+		int nodo;
+		recv(socket_fs,&nodo,sizeof(int),0);
+
+		nodo = 32;
+		send(socket_fs,&nodo,sizeof(int),0);
+
+		int tamanioData = sizeof(int) + strlen(ip_nodo) + 1 + sizeof(int) + strlen(string_itoa(puerto_nodo)) + 1 + sizeof(int);
+
+		recv(socket_fs,&nodo,sizeof(int),0);
+		printf("%i\n",tamanioData);
+
+		send(socket_fs, &tamanioData, sizeof(int), 0);
+		mensaje = serializarIP_PUERTO(ip_nodo, string_itoa(puerto_nodo), tamanioData);
+		send(socket_fs,mensaje,tamanioData,0);
+
+		printf("mande todo lo que tenia que mandar\n");
+
+		recv(socket_fs, &id_nodo, sizeof(int), 0);
+
+		nuevoArchivo = fopen("/home/utnso/git/tp-2015-1c-machistons/Configuracion/nodo.conf","w");
+
+		fputs("PUERTO_FS=3001\n",nuevoArchivo);
+
+		fputs("IP_FS=",nuevoArchivo);
+		fputs(ip_fs,nuevoArchivo);
+		fputc('\n',nuevoArchivo);
+
+		fputs("ARCHIVO_BIN=",nuevoArchivo);
+		fputs(archivo_bin,nuevoArchivo);
+		fputc('\n',nuevoArchivo);
+
+		fputs("DIR_TEMP=",nuevoArchivo);
+		fputs(dir_temp,nuevoArchivo);
+		fputc('\n',nuevoArchivo);
+
+		fputs("NODO_NUEVO=NO\n",nuevoArchivo);
+		nodo_nuevo = "NO";
+
+		fputs("IP_NODO=",nuevoArchivo);
+		fputs(ip_nodo,nuevoArchivo);
+		fputc('\n',nuevoArchivo);
+
+		fputs("PUERTO_NODO=6000\n",nuevoArchivo);
+
+		fputs("ID_NODO=",nuevoArchivo);
+		fputs(id_nodo,nuevoArchivo);
+		fputc('\n',nuevoArchivo);
+
+		fclose(nuevoArchivo);
+
+	}else{
+
+		int nodo=48;
+		send(socket_fs,&nodo,sizeof(int),0);
+
+		send(socket_fs,&id_nodo, sizeof(int), 0);
+
+	}
+
+	free(mensaje);
+
 	printf("%i\n",socket_fs);
 	pthread_create(&hiloFS, NULL, &atenderNFS, (void *)socket_fs);
 
--- Nodo/src/funcionesParaEscribir.c
+++ Nodo/src/funcionesParaEscribir.c
@@ -12,12 +12,12 @@ int escribirScript(char* script_virtual,char* dir_temp, int comando){
 	char* direccion=dir_temp;
 
 	if(comando==1){
-		strcat(direccion,"/map.txt");
+		strcat(direccion,"/map.sh");
 		fileMapper=fopen(dir_temp,"w");
 		fputs(script_mapper,fileMapper);
 		fclose(fileMapper);
 	}else{
-		strcat(direccion,"/reducer.txt");
+		strcat(direccion,"/reducer.sh");
 		fileReducer=fopen(dir_temp,"w");
 		fputs(script_reducer,fileReducer);
 		fclose(fileReducer);
