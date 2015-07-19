adb28ce3fac08b27268876d437ea40f5e3a393ab
matiasmargu <matias.margu@gmail.com>
2015-07-18 22:03:03 -0300

    Saco el job qe no sirve parte 2

--- Job/src/funcionesJob.h
+++ Job/src/funcionesJob.h
@@ -1,57 +1,72 @@
 /*
- * funciones.h
+ * funcionesJob2.h
  *
- *  Created on: 9/5/2015
+ *  Created on: 9/7/2015
  *      Author: utnso
  */
 
-#ifndef FUNCIONES_H_
-#define FUNCIONES_H_
+#ifndef JOB_SRC_FUNCIONESJOB2_H_
+#define JOB_SRC_FUNCIONESJOB2_H_
 
-#include <sys/socket.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
-#include <pthread.h>
-#include <socket/socket.h>
-#include <netdb.h>
-#include <unistd.h>
 #include <string.h>
-#include <assert.h>
-#include <string.h>
-#include <./commons/config.h>
-#include <./commons/log.h>
-#include <./commons/string.h>
-#include <./commons/collections/list.h>
 #include <sys/types.h>
 #include <netdb.h>
 #include <unistd.h>
-#include <pthread.h>
 #include <stdint.h>
+#include <assert.h>
+#include <./commons/config.h>
+#include <./commons/log.h>
+#include <./commons/string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <socket/socket.h>
+#include <./commons/collections/list.h>
+#include <./commons/bitarray.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 
-typedef struct{
-	char* archivo;
-}t_charpuntero;
-
-typedef struct{
-	t_marta_job Marta_Job;
-	int	socketMarta;
-	int	numeroDeBloque;
-	int idNodo;
-	t_job_nodo Job_Nodo;
-	}t_conectarseAlNodo;
 
 
 t_log* logger; // Log Global
+pthread_mutex_t mutex;
 
-void conectarseAlNodo(t_conectarseAlNodo CAN);
-char* serializarRutina(t_job_nodo* jn, int tamanioTotal);
-char* serializarJob_Nodo(t_job_nodo *job_nodo, int tamanioTotal);
-char* serializar_charpuntero(t_charpuntero *nombre, int tamanioTotal);
-char* serializar_job_marta(t_job_marta *job_marta, int tamanioTotal);
-int recive_y_deserialisa_marta_job(t_marta_job *bloque, int socket, uint32_t tamanioTotal);
-void liberarMensaje(char *);
-void *mandarPruebaAMarta(void*arg);
-void *mandarPrueba2ANodo(void*arg);
 
-#endif /* FUNCIONES_H_ */
+
+
+typedef struct{
+	int bloque;
+	int socketNodo;
+	char* nombreArchivoResultado;
+    int socketMarta;
+    int idNodo;
+}t_hilo_map;
+
+typedef struct{
+	int socketNodo;
+	t_list* archivos;
+	char* nombreArchivoResultado;
+	int socketMarta;
+	int idNodo;
+}t_hilo_reduce;
+
+int recive_y_deserialisa_marta_job_mapper(t_marta_job_map *bloque, int socket, uint32_t tamanioTotal);
+void* mapearBloque(t_hilo_map *structRecibido);
+char* serializar_job_marta(t_job_marta_map *job_marta, int tamanioTotal);
+void* reducirArchivos(t_hilo_reduce *structRecibido);
+int recive_y_deserialisa_marta_job_reduce(t_marta_job_reduce *bloque, int socket, uint32_t tamanioTotal);
+char* mapearAMemoriaVirtual(char* archivo);
+char *serializar_job_marta_reduce(t_job_marta_reduce *job_marta,int tamanioTotal);
+
+
+#endif /* JOB_SRC_FUNCIONESJOB2_H_ */
