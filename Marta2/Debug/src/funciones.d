6a16ae4c31836b70063a1515da03364f90517dad
MatiNav <matiasnavarroezequiel@gmail.com>
2015-07-23 19:45:02 -0300

    tocando a marta

--- Marta2/Debug/src/funciones.d
+++ Marta2/Debug/src/funciones.d
@@ -1,36 +1,36 @@
 src/funciones.d: ../src/funciones.c ../src/funciones.h \
- /home/utnso/git/tp-2015-1c-machistons/funciones/./commons/config.h \
- /home/utnso/git/tp-2015-1c-machistons/funciones/./commons/collections/dictionary.h \
- /home/utnso/git/tp-2015-1c-machistons/funciones/./commons/collections/node.h \
- /home/utnso/git/tp-2015-1c-machistons/funciones/./commons/log.h \
- /home/utnso/git/tp-2015-1c-machistons/funciones/./commons/string.h \
- /home/utnso/git/tp-2015-1c-machistons/funciones/socket/socket.h \
- /home/utnso/git/tp-2015-1c-machistons/funciones/socket/../commons/log.h \
- /home/utnso/git/tp-2015-1c-machistons/funciones/socket/../commons/collections/list.h \
- /home/utnso/git/tp-2015-1c-machistons/funciones/socket/../commons/collections/node.h \
- /home/utnso/git/tp-2015-1c-machistons/funciones/./commons/collections/list.h \
- /home/utnso/git/tp-2015-1c-machistons/funciones/./commons/bitarray.h
+ /home/utnso/tp-2015-1c-machistons/funciones/./commons/config.h \
+ /home/utnso/tp-2015-1c-machistons/funciones/./commons/collections/dictionary.h \
+ /home/utnso/tp-2015-1c-machistons/funciones/./commons/collections/node.h \
+ /home/utnso/tp-2015-1c-machistons/funciones/./commons/log.h \
+ /home/utnso/tp-2015-1c-machistons/funciones/./commons/string.h \
+ /home/utnso/tp-2015-1c-machistons/funciones/socket/socket.h \
+ /home/utnso/tp-2015-1c-machistons/funciones/socket/../commons/log.h \
+ /home/utnso/tp-2015-1c-machistons/funciones/socket/../commons/collections/list.h \
+ /home/utnso/tp-2015-1c-machistons/funciones/socket/../commons/collections/node.h \
+ /home/utnso/tp-2015-1c-machistons/funciones/./commons/collections/list.h \
+ /home/utnso/tp-2015-1c-machistons/funciones/./commons/bitarray.h
 
 ../src/funciones.h:
 
-/home/utnso/git/tp-2015-1c-machistons/funciones/./commons/config.h:
+/home/utnso/tp-2015-1c-machistons/funciones/./commons/config.h:
 
-/home/utnso/git/tp-2015-1c-machistons/funciones/./commons/collections/dictionary.h:
+/home/utnso/tp-2015-1c-machistons/funciones/./commons/collections/dictionary.h:
 
-/home/utnso/git/tp-2015-1c-machistons/funciones/./commons/collections/node.h:
+/home/utnso/tp-2015-1c-machistons/funciones/./commons/collections/node.h:
 
-/home/utnso/git/tp-2015-1c-machistons/funciones/./commons/log.h:
+/home/utnso/tp-2015-1c-machistons/funciones/./commons/log.h:
 
-/home/utnso/git/tp-2015-1c-machistons/funciones/./commons/string.h:
+/home/utnso/tp-2015-1c-machistons/funciones/./commons/string.h:
 
-/home/utnso/git/tp-2015-1c-machistons/funciones/socket/socket.h:
+/home/utnso/tp-2015-1c-machistons/funciones/socket/socket.h:
 
-/home/utnso/git/tp-2015-1c-machistons/funciones/socket/../commons/log.h:
+/home/utnso/tp-2015-1c-machistons/funciones/socket/../commons/log.h:
 
-/home/utnso/git/tp-2015-1c-machistons/funciones/socket/../commons/collections/list.h:
+/home/utnso/tp-2015-1c-machistons/funciones/socket/../commons/collections/list.h:
 
-/home/utnso/git/tp-2015-1c-machistons/funciones/socket/../commons/collections/node.h:
+/home/utnso/tp-2015-1c-machistons/funciones/socket/../commons/collections/node.h:
 
-/home/utnso/git/tp-2015-1c-machistons/funciones/./commons/collections/list.h:
+/home/utnso/tp-2015-1c-machistons/funciones/./commons/collections/list.h:
 
-/home/utnso/git/tp-2015-1c-machistons/funciones/./commons/bitarray.h:
+/home/utnso/tp-2015-1c-machistons/funciones/./commons/bitarray.h:
--- Marta2/src/Marta2.c
+++ Marta2/src/Marta2.c
@@ -38,6 +38,268 @@ typedef struct {
     char* nombreArchivoResultado;
 }t_archivosAReducirPorNodo;
 
+typedef struct {
+	int contador;
+	int idNodo;
+}t_contadorNodo;
+
+
+void planificarSincombiner(){
+
+	t_lista_job2 *tabla;
+	t_lista_job2 *tabla2;
+	t_lista_job2* tabla3;
+	t_tablaProcesos_porJob2 *campoDeTabla;
+	t_tablaProcesos_porJob2 *campoDeTabla2;
+	t_tablaProcesos_porJob2 *campoDeTabla3;
+	t_tablaProcesos_porJob2 *campoDeTabla4;
+	t_tablaProcesos_porJob2 *campoDeTabla5;
+	t_tablaProcesos_porJob2 *campoDeTabla6;
+	t_list* lista_job_tabla;
+	//REDUCE FINAL
+	t_job_marta_reduce job_marta;
+	int contador2,r,l,tamanioArecibir,p,tamanioStruct, tamanioListaFinal,m;
+	t_archivosAReducirPorNodo *campoDeListaArchivosAReducirPorNodo;
+	t_archivosAReducirPorNodo *campoDeListaArchivosAReducirPorNodo2;
+	t_archivosAReducirPorNodo * campoDeListaArchivosAReducirPorNodo3;
+	t_list* listaFinalDeArchivosAReducir;
+	t_marta_job_reduce antesDeSerializar;
+	char* structParaJob;
+	char* archivoA;
+	contador2 = 0;
+
+
+	// para probar
+	int tamanioTabla,tamanioCampoDeTabla,tamanioNodo, contador;
+	tamanioTabla=sizeof(int);
+	tamanioCampoDeTabla= sizeof(int) * 3 + sizeof(char);
+	tamanioNodo = sizeof(char) * 3 + sizeof(int);
+
+	tabla = malloc(tamanioTabla);
+	tabla2 = malloc(tamanioTabla);
+	tabla3 = malloc(tamanioTabla);
+	campoDeTabla = malloc(tamanioCampoDeTabla);
+	campoDeTabla2 = malloc(tamanioCampoDeTabla);
+	campoDeTabla3 = malloc(tamanioCampoDeTabla);
+	campoDeTabla4 = malloc(tamanioCampoDeTabla);
+	campoDeTabla5 = malloc(tamanioCampoDeTabla);
+	campoDeTabla6 = malloc(tamanioCampoDeTabla);
+	tabla->idJob=4;
+	tabla->tabla_procesos = list_create();
+
+	tabla2->idJob=7854;
+	tabla2->tabla_procesos = list_create();
+
+	tabla3->idJob=5;
+
+	tabla3->tabla_procesos = list_create();
+
+
+	campoDeTabla->bloque_archivo = 5;
+	campoDeTabla->estado = 3;
+	campoDeTabla->id_nodo = 4;
+	campoDeTabla->nombre_archivo_resultado = "campoTabla1";
+
+	campoDeTabla2->bloque_archivo = 2;
+	campoDeTabla2->estado = 0;
+	campoDeTabla2->id_nodo = 2;
+	campoDeTabla2->nombre_archivo_resultado = "campoTabla2";
+
+	campoDeTabla3->bloque_archivo = 4;
+	campoDeTabla3->estado = 1;
+	campoDeTabla3->id_nodo = 7;
+	campoDeTabla3->nombre_archivo_resultado = "campoTabla3";
+
+	campoDeTabla4->bloque_archivo = 0;
+	campoDeTabla4->estado = 1;
+	campoDeTabla4->id_nodo = 54;
+	campoDeTabla4->nombre_archivo_resultado = "campoTabla4";
+
+	campoDeTabla5->bloque_archivo = 14;
+	campoDeTabla5->estado = 2;
+	campoDeTabla5->id_nodo = 54;
+	campoDeTabla5->nombre_archivo_resultado = "campoTabla5";
+
+	campoDeTabla6->bloque_archivo = 21;
+	campoDeTabla6->estado = 3;
+	campoDeTabla6->id_nodo = 54;
+	campoDeTabla6->nombre_archivo_resultado = "campoTabla6";
+
+
+	list_add(tabla->tabla_procesos,campoDeTabla);
+	list_add(tabla->tabla_procesos,campoDeTabla2);
+	list_add(tabla2->tabla_procesos,campoDeTabla3);
+	list_add(tabla2->tabla_procesos,campoDeTabla4);
+	list_add(tabla3->tabla_procesos,campoDeTabla5);
+	list_add(tabla3->tabla_procesos,campoDeTabla6);
+
+	lista_job_tabla = list_create();
+	list_add(lista_job_tabla,tabla);
+	list_add(lista_job_tabla,tabla2);
+	list_add(lista_job_tabla,tabla3);
+
+	lista_nodos_estado = list_create();
+	t_nodo2 *nodo1;
+	t_nodo2 *nodo2;
+	t_nodo2 *nodo3;
+	t_nodo2 *nodo4;
+	t_nodo2 *nodo5;
+	t_nodo2 *nodo6;
+
+	nodo1=malloc(tamanioNodo);
+	nodo2=malloc(tamanioNodo);
+	nodo3=malloc(tamanioNodo);
+	nodo4=malloc(tamanioNodo);
+	nodo5=malloc(tamanioNodo);
+	nodo6=malloc(tamanioNodo);
+
+
+	nodo1->id_nodo = 4;
+	nodo1->ip_nodo = "44.251";
+	nodo1->puerto_nodo = "887";
+	nodo2->id_nodo = 2;
+	nodo2->ip_nodo = "44.250";
+	nodo2->puerto_nodo = "886";
+	nodo3->id_nodo = 7;
+	nodo3->ip_nodo = "44.252";
+	nodo3->puerto_nodo = "885";
+	nodo4->id_nodo = 54;
+	nodo4->ip_nodo = "44.253";
+	nodo4->puerto_nodo = "884";
+	nodo5->id_nodo = 23;
+	nodo5->ip_nodo = "44.251";
+	nodo5->puerto_nodo = "883";
+	nodo6->id_nodo = 44;
+	nodo6->ip_nodo = "44.254";
+	nodo6->puerto_nodo = "882";
+
+	list_add(lista_nodos_estado,nodo1);
+	list_add(lista_nodos_estado,nodo2);
+	list_add(lista_nodos_estado,nodo3);
+	list_add(lista_nodos_estado,nodo4);
+	list_add(lista_nodos_estado,nodo5);
+	list_add(lista_nodos_estado,nodo6);
+
+	int a,b,c,d,e,aux,variableParaResultadoReduce,tamanioListaJobTabla,tamanioListaTablaDeProcesosPorJob,tamanioListaArchivosAReducirPorNodo;
+	int tamanioListaDeNodos,tamanio,tamanioArchivosaReducir,tamanioListaContadoresNodo;
+	t_archivosAReducirPorNodo *campoDeUnNodo;
+	t_archivosAReducirPorNodo * campoParaLiberar;
+	t_marta_job_reduce structAserializar;
+	int handshakeJob, enteroPrueba,tamanioAenviar,tamanioTotalVector, socketJob;
+	char* archivo1;
+	char* structAEnviarAJob;
+	t_lista_job2 *campoDeLaLista;
+	campoDeLaLista = malloc(tamanioTabla);
+	t_tablaProcesos_porJob2 *campoDeLaListaTablaDeProcesos;
+	campoDeLaListaTablaDeProcesos = malloc(tamanioCampoDeTabla);
+	t_list * lista_archivosAReducirPorNodo;
+	t_archivosAReducirPorNodo *campoArchivosAReducirPorNodo;
+	campoArchivosAReducirPorNodo= malloc(tamanioCampoDeTabla);
+	t_archivosAReducirPorNodo *campoAAgregarAListaReducirPorNodo;
+	t_archivosAReducirPorNodo *campoParaAumentarContadores;
+	t_contadorNodo * contadorNodo;
+	t_list * listaDeContadoresNodo;
+	listaDeContadoresNodo = list_create();
+	t_nodo2 *campoDeListaDeNodo;
+	campoDeListaDeNodo = malloc(tamanioNodo);
+	lista_archivosAReducirPorNodo = list_create();
+	list_clean(lista_archivosAReducirPorNodo);
+
+	for(a=0;a<tamanioListaJobTabla;a++){
+		campoDeLaLista=list_get(lista_job_tabla,a);
+		tamanioListaTablaDeProcesosPorJob = list_size(campoDeLaLista->tabla_procesos);
+
+		for(b=0;b<tamanioListaTablaDeProcesosPorJob;b++){
+			campoDeLaListaTablaDeProcesos = list_get(campoDeLaLista->tabla_procesos,b);
+			tamanioListaContadoresNodo = list_size(listaDeContadoresNodo);
+			if(tamanioListaArchivosAReducirPorNodo == 0){
+				campoAAgregarAListaReducirPorNodo = malloc(tamanioCampoDeTabla);
+				campoAAgregarAListaReducirPorNodo->idNodo =  campoDeLaListaTablaDeProcesos->id_nodo;
+				campoAAgregarAListaReducirPorNodo->archivosAReducir = list_create();
+				list_add(campoAAgregarAListaReducirPorNodo->archivosAReducir,campoDeLaListaTablaDeProcesos->nombre_archivo_resultado);
+				asprintf(&campoAAgregarAListaReducirPorNodo->nombreArchivoResultado,"%s%i","archivo",variableParaResultadoReduce);
+				variableParaResultadoReduce ++;
+				// buscamos el ip y el puerto nodo
+				tamanioListaDeNodos =list_size(lista_nodos_estado);
+				aux=0;
+				for(d=0;d<tamanioListaDeNodos && aux==0;d++){
+					campoDeListaDeNodo = list_get(lista_nodos_estado,d);
+					if(campoDeListaDeNodo->id_nodo==campoDeLaListaTablaDeProcesos->id_nodo){
+						campoAAgregarAListaReducirPorNodo->ipNodo =  campoDeListaDeNodo->ip_nodo;
+						campoAAgregarAListaReducirPorNodo->puertoNodo =  campoDeListaDeNodo->puerto_nodo;
+						aux=1;
+
+					}
+				}// agrega el campo a la lista
+
+				list_add(lista_archivosAReducirPorNodo,campoAAgregarAListaReducirPorNodo);
+				campoAAgregarAListaReducirPorNodo = list_get(lista_archivosAReducirPorNodo,e);
+				printf("idnodo %i ipnodo %s puertonodo %s nombreDeArchivoResultado %s\n",campoAAgregarAListaReducirPorNodo->idNodo,campoAAgregarAListaReducirPorNodo->ipNodo,campoAAgregarAListaReducirPorNodo->puertoNodo,campoAAgregarAListaReducirPorNodo->nombreArchivoResultado);
+				e++;
+			}
+			else{
+				aux=0;
+				for(c=0;c <tamanioListaArchivosAReducirPorNodo && aux ==0;c++){
+					campoArchivosAReducirPorNodo = list_get(lista_archivosAReducirPorNodo,c);
+					if(campoArchivosAReducirPorNodo->idNodo == campoDeLaListaTablaDeProcesos->id_nodo){
+						list_add(campoArchivosAReducirPorNodo->archivosAReducir,campoDeLaListaTablaDeProcesos->nombre_archivo_resultado);
+						aux =1;
+					}
+				}
+				if(aux==0){//cuando no esta el id del nodo, entonces tiene que agregar todo directo
+					printf("Aca deberia entrar 4 veces \n");
+					campoAAgregarAListaReducirPorNodo = malloc(tamanioCampoDeTabla);
+					campoAAgregarAListaReducirPorNodo->idNodo =  campoDeLaListaTablaDeProcesos->id_nodo;
+					campoAAgregarAListaReducirPorNodo->archivosAReducir = list_create();
+					list_add(campoAAgregarAListaReducirPorNodo->archivosAReducir,campoDeLaListaTablaDeProcesos->nombre_archivo_resultado);
+
+					asprintf(&campoAAgregarAListaReducirPorNodo->nombreArchivoResultado,"%s%i","archivo",variableParaResultadoReduce);
+					variableParaResultadoReduce ++;
+					// buscamos el ip y el puerto nodo
+					tamanioListaDeNodos =list_size(lista_nodos_estado);
+					aux=0;
+					for(d=0;d<tamanioListaDeNodos && aux==0;d++){
+						campoDeListaDeNodo = list_get(lista_nodos_estado,d);
+						if(campoDeListaDeNodo->id_nodo==campoDeLaListaTablaDeProcesos->id_nodo){
+							campoAAgregarAListaReducirPorNodo->ipNodo =  campoDeListaDeNodo->ip_nodo;
+							campoAAgregarAListaReducirPorNodo->puertoNodo =  campoDeListaDeNodo->puerto_nodo;
+							aux=1;
+
+						}
+					}// agrega el campo a la lista
+					list_add(lista_archivosAReducirPorNodo,campoAAgregarAListaReducirPorNodo);
+					campoAAgregarAListaReducirPorNodo = list_get(lista_archivosAReducirPorNodo,e);
+					printf("idnodo %i ipnodo %s puertonodo %s nombreDeArchivoResultado %s\n",campoAAgregarAListaReducirPorNodo->idNodo,campoAAgregarAListaReducirPorNodo->ipNodo,campoAAgregarAListaReducirPorNodo->puertoNodo,campoAAgregarAListaReducirPorNodo->nombreArchivoResultado);
+					e++;
+
+				}
+			}
+		}
+	}
+
+	 tamanio = list_size(lista_archivosAReducirPorNodo);
+	 contadorNodo = malloc(sizeof(int) * 2 );
+	 contadorNodo->contador = 0;
+	 for(e=0;e<tamanio;e++){
+		 campoParaAumentarContadores = list_get(lista_archivosAReducirPorNodo,e);
+		 a=list_size(campoParaAumentarContadores->archivosAReducir);
+
+		 if (a > contadorNodo->contador) {
+			 contadorNodo->idNodo = campoParaAumentarContadores->idNodo;
+		 }
+
+	 }
+
+	 printf("\n%i\n",contadorNodo->contador);
+
+
+
+
+
+
+
+
+/*
 
 int main(void){
 
@@ -277,235 +539,11 @@ int main(void){
          free(campoDeLaListaTablaDeProcesos);
          free(campoArchivosAReducirPorNodo);
          free(campoDeListaDeNodo);
-         /*
-		//
-	    int a,b,c,d,aux=0,tamanioListaJobTabla,tamanioListaTablaDeProcesosPorJob,tamanioListaArchivosAReducirPorNodo;
-		int tamanioListaDeNodos,variableDelRealloc=0,aux2,tamanioListaNueva;
-		char* archivosAReducirTemporales;
-	    t_lista_job2 *campoDeLaLista;
-	    campoDeLaLista = malloc(tamanio23*5);
-		t_tablaProcesos_porJob2 *campoDeLaListaTablaDeProcesos;
-        campoDeLaListaTablaDeProcesos = malloc(tamanio23*5);
-	    t_archivosAReducirPorNodo *lista_archivosAReducirPorNodoParcial;
-	    t_archivosAReducirPorNodo *lista_archivosAReducirPorNodoFinal;
-	    t_archivosAReducirPorNodo *campoArchivosAReducirPorNodo;
-        campoArchivosAReducirPorNodo= malloc(tamanio23*5);
-	    t_archivosAReducirPorNodo *campoAAgregarAListaReducirPorNodo;
-	    campoAAgregarAListaReducirPorNodo = malloc(tamanio23*5);
-	    t_nodo2 *campoDeListaDeNodo;
-	    campoDeListaDeNodo = malloc(tamanio23*5);
-		lista_archivosAReducirPorNodoParcial = malloc(sizeof(t_archivosAReducirPorNodo));
+return EXIT_SUCCESS;
 
-		// lo que buscamos es recorrer cada campo de la lista para ordenar por id de nodo la lista de archivos a
-		//reducir
-		tamanioListaJobTabla=list_size(lista_job_tabla);
-		printf("tamanioListaJobTabla %i\n",tamanioListaJobTabla);
-		tamanioListaNueva=sizeof(int)*2 + sizeof(char)*4;
-
-		for(a=0;a<tamanioListaJobTabla;a++){
-			campoDeLaLista=list_get(lista_job_tabla,a);
-			tamanioListaTablaDeProcesosPorJob = list_size(campoDeLaLista->tabla_procesos);
-			for(b=0;b<tamanioListaTablaDeProcesosPorJob;b++){
-				campoDeLaListaTablaDeProcesos = list_get(campoDeLaLista->tabla_procesos,b);
-				if(aux==0){ // si es la primera vez hay que hacer un malloc para inicializar la lista que vamos a hacer nosotros, e inicializar la de archivos a reducir
-					lista_archivosAReducirPorNodoParcial = malloc(tamanioListaNueva);
-					lista_archivosAReducirPorNodoParcial[0].idNodo =campoDeLaListaTablaDeProcesos->id_nodo;
-					lista_archivosAReducirPorNodoParcial[0].archivosAReducir=malloc(sizeof(char));
-					lista_archivosAReducirPorNodoParcial[0].archivosAReducir[0]=campoDeLaListaTablaDeProcesos->nombre_archivo_resultado;
-					lista_archivosAReducirPorNodoParcial[0].cantidadDeArchivosAReducir=1;
-					asprintf(&lista_archivosAReducirPorNodoParcial[0].nombreArchivoResultado,"%s%i","archivo",aux);
-					tamanioListaDeNodos =list_size(lista_nodos_estado);
-					aux2=0;
-					for(d=0;d<tamanioListaDeNodos && aux2==0;d++){
-						campoDeListaDeNodo = list_get(lista_nodos_estado,d);
-						if(campoDeListaDeNodo->id_nodo==campoDeLaListaTablaDeProcesos->id_nodo){
-							lista_archivosAReducirPorNodoParcial[0].ipNodo=  campoDeListaDeNodo->ip_nodo;
-							lista_archivosAReducirPorNodoParcial[0].puertoNodo =  campoDeListaDeNodo->puerto_nodo;
-							aux2=1;
-						}
-					}
-					aux ++;
-					printf("tiene que ser solo uno\n");
-				}
-				else{ // aca ya no es la primera, hay que ver si el id del nodo es el mismo o no a alguno, si es el mismo hay que hacer un realloc en los archivos a reducir
-					//si no es el mismo hay que hacer un realloc en la lista que hacemos nosotros y hay que inicializar(hacer un malloc) en la lista de archivos a reducir
-					aux2=0;
-					for(c=0;c<aux && aux2==0;c++){// aux va a contener cuantas posiciones del vector hay
-						if(lista_archivosAReducirPorNodoParcial[c].idNodo==campoDeListaDeNodo->id_nodo){
-							// aca la va a recorrer y si encuentra un id que coincide ahi agrega el archivo a reducir
-							lista_archivosAReducirPorNodoParcial[c].cantidadDeArchivosAReducir ++;
-							archivosAReducirTemporales = realloc(lista_archivosAReducirPorNodoParcial[c].archivosAReducir, lista_archivosAReducirPorNodoParcial[c].cantidadDeArchivosAReducir * sizeof(char));
-							/* Si hubo un error
-							if (archivosAReducirTemporales == NULL)
-							{
-							printf("Error al reservar memoria\n");
-							/* Liberamos la memoria del puntero
-							y terminamos el programa
-							free(lista_archivosAReducirPorNodoParcial[c].archivosAReducir);
-							exit(1);
-							}
-							lista_archivosAReducirPorNodoParcial[c].archivosAReducir= archivosAReducirTemporales;
-							lista_archivosAReducirPorNodoParcial[c].archivosAReducir[(lista_archivosAReducirPorNodoParcial[c].cantidadDeArchivosAReducir - 1 )] = campoDeLaListaTablaDeProcesos->nombre_archivo_resultado;
-							aux2=1;
-						}
-					} si aux2 es igual a cero, significa que nunca entro por lo que ahi vamos a tener que hacer el realloc y el malloc
-					if(aux2==0){
-						lista_archivosAReducirPorNodoFinal = realloc(lista_archivosAReducirPorNodoParcial,(aux+1)*tamanioListaNueva);
-						if (lista_archivosAReducirPorNodoFinal == NULL) {
-							printf("Error al reservar memoria\n");
-							/* Liberamos la memoria del puntero
-													y terminamos el programa
-							free(lista_archivosAReducirPorNodoFinal);
-							exit(1);
-						}
-
-						lista_archivosAReducirPorNodoParcial[aux].idNodo =campoDeLaListaTablaDeProcesos->id_nodo;
-						lista_archivosAReducirPorNodoParcial[aux].archivosAReducir=malloc(sizeof(char));
-						lista_archivosAReducirPorNodoParcial[aux].archivosAReducir[0]=campoDeLaListaTablaDeProcesos->nombre_archivo_resultado;
-						lista_archivosAReducirPorNodoParcial[aux].cantidadDeArchivosAReducir=1;
-						asprintf(&lista_archivosAReducirPorNodoParcial[aux].nombreArchivoResultado,"%s%i","archivo",aux);
-						tamanioListaDeNodos =list_size(lista_nodos_estado);
-						aux2=0;
-						for(d=0;d<tamanioListaDeNodos && aux2==0;d++){
-							campoDeListaDeNodo = list_get(lista_nodos_estado,d);
-							if(campoDeListaDeNodo->id_nodo==campoDeLaListaTablaDeProcesos->id_nodo){
-								lista_archivosAReducirPorNodoParcial[aux].ipNodo=  campoDeListaDeNodo->ip_nodo;
-								lista_archivosAReducirPorNodoParcial[aux].puertoNodo =  campoDeListaDeNodo->puerto_nodo;
-								aux2=1;
-							}
-						}
-						aux ++;
-					}
-
-				}
-			}
-		}
-
-		for(d=0;d<aux;d++){
-			printf("vector[%i]:\nidNodo %i\n ipNodo %c\n puertoNodo %c\n nombreArchivoResultado %c \n\n ",lista_archivosAReducirPorNodoFinal[aux].idNodo,lista_archivosAReducirPorNodoFinal[aux].ipNodo,lista_archivosAReducirPorNodoFinal[aux].puertoNodo,lista_archivosAReducirPorNodoFinal[aux].nombreArchivoResultado);
-		}
-
-		free(lista_archivosAReducirPorNodoParcial);
-
-/*
-	*/
-
-
-
-
-
-
-
-
-
-
-
-
-	/*
-	char* rutaArchivoConfiguracion;
-	t_config* archivoConfiguracion;
-	int fdmax, listener, newfd, yes = 1, addrlen, i,puerto_job,socketjob,entero;
-	int martafd, entero2, entero3; //Socket de coneccion con Marta
-	struct sockaddr_in serveraddr;
-	struct sockaddr_in clientaddr;
-	fd_set master;
-	fd_set read_fds;
-	pthread_t hilo_job;
-	t_respuesta_reduce respuestaReduce;
-
-	listaJobs = list_create();
-
-	rutaArchivoConfiguracion = "/home/utnso/git/tp-2015-1c-machistons/Configuracion/marta.conf";
-	archivoConfiguracion = config_create(rutaArchivoConfiguracion);
-	puerto_job = config_get_int_value(archivoConfiguracion, "PUERTO_MARTA") ;
-
-	listaArchivos = list_create();
-
-	FD_ZERO(&master);
-	FD_ZERO(&read_fds);
-
-	listener = socket(AF_INET, SOCK_STREAM, 0);
-	setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
-
-	serveraddr.sin_family = AF_INET;
-	serveraddr.sin_addr.s_addr = INADDR_ANY;
-	serveraddr.sin_port = htons(puerto_job);
-	memset(&(serveraddr.sin_zero), '\0', 8);
-	bind(listener, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
-	listen(listener, 10);
-	FD_SET(listener, &master);
-
-	fdmax = listener;
-	logger = log_create("LOG_MARTA", "log_marta" ,false, LOG_LEVEL_INFO);
-
-	for (;;){
-	read_fds = master;
-	select(fdmax+1, &read_fds, NULL, NULL, NULL);
-	printf("select activo\n");
-	for(i = 0; i <= fdmax; i++)
-	{
-	    if(FD_ISSET(i, &read_fds))
-	    {
-	    	if(i == listener)
-	    	{
-				addrlen = sizeof(clientaddr);
-				if((newfd = accept(listener, (struct sockaddr *)&clientaddr, &addrlen)) == -1)
-				{
-				}
-				else
-				{
-					FD_SET(newfd, &master);
-					if(newfd > fdmax)
-					{
-						fdmax = newfd;
-					}
-				}
-	    	}
-	    	else
-	    	{
-	    		if((recv(i, &entero, sizeof(int),0 )) <= 0)
-	    		{
-	    			if (i == martafd){
-	    				log_info(logger,"Se ha perdido la coneccion con Job");
-	    				//se callo el job
-	    			}
-	    			else{
-
-	    				// se callo un nodo y tengo que ponerlo como no disponible
-	    			}
-	    			close(i); // Coneccion perdida
-	    			FD_CLR(i, &master);
-	    		}
-	    		else{
-	    			switch(entero){
-	    				case 1: // ACA EL JOB LE PASA LISTA DE ARCHIVOS Y EL COMBINER
-	    					socketjob = i;
-	    					printf("se conecto el job\n");
-
-	    					pthread_create(&hilo_job, NULL, recibirListaDeArchivos,(void *)socketjob);
-	    					break;
-	    				case 2: //ACA EL JOB LE PASA EL RESULTADO DEL MAP
-	    					//socketJob = i;
-	    					printf("aca recibe resultado map\n");
-	    				//	pthread_create(&hilo_job, NULL, planificarReduce, (void*) socketJob);
-
-	    					break;
-
-	    				case 3://ACA EL JOB LE PASA EL RESULTADO DEL REDUCE
-
-	    					//pthread_create(&hilo_job, NULL, recibirResultadoReduce,(void *)socketjob);
-	    					break;
-
-	    			}
-	    		}
-	    		}
-	    	}
-		}
-		}*/
-		return EXIT_SUCCESS;
 }
-
-
+*/
+}
 
 
 
