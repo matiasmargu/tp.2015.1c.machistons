259ef1a5121fb456482107bf972bc02de896414f
mantovanileandro <mantovani.leandro@hotmail.com>
2015-07-24 17:39:57 -0300

    Update planificacion_map.c

--- Marta/src/planificacion_map.c
+++ Marta/src/planificacion_map.c
@@ -6,7 +6,7 @@
  */
 
 #include "librerias_y_estructuras.h"
-
+/*
 int calcularCantidadDeBloques(t_list* lista_archivos){
 	int cantidad,j,h,k,p;
 	t_archivo* archivo;
@@ -41,44 +41,91 @@ void inicializarBitarray(t_bitarray *bitmap, int tamanio){
 	}
 }
 
-int buscarPorNodo(int idNodo, int nodos_activos[], int pos){
-	return 1;
+int get_nodo_pos(int nodos_activos[], int id){
+	int i,pos;
+	//printf("ID DEL NODO QUE ME LLEGA: %i\n",id);
+	for(i=0;i<cant_nodos;i++){
+		if(nodos_activos[i] == id) pos = i;
+		//printf("NODO ACTIVO: %i\n",nodos_activos[i]);
+	}
+	return pos;
 }
 
+/*
 t_cargaBitarray_aux *armarVectorDeBitarray(t_cargaBitarray_aux *vectorDeBitArrays, int cantidadDeNodos, int nodos_activos[], int *sub_indice){
 	//int cantidadDeBloquesTotales = calcularCantidadDeBloques(lista_archivos);
-	t_archivo *un_archivo;
+	t_archivo *un_archivo = malloc(sizeof(t_archivo));
+	t_bloque *bloque = malloc(sizeof(t_bloque));
+	char *un_nombre;
 	// OJO CON ESTO, NO ESTOY SEGURO SI NO HAY QUE PASARLO CARGADO CON 0 PREVIAMENTE
-	char *un_nombre = malloc(cantidadDeNodos * sizeof(char));
 	//seteo los bitarrays
-	int j,h, k;
+	int j,h,k,pos,nodo_id;
 	(*sub_indice) = 0;
-	printf("nodos control 2\n");
+	printf("-------------------------Estructura del bitarray--------------------------------\n");
 	printf("CANTIDAD DE ARCHIVOS: %i\n",list_size(lista_archivos));
 	for(j=0 ; j< list_size(lista_archivos);j++){
-		printf("nodos control 3\n");
+		//printf("nodos control 3\n");
+		printf("LIST ARCHIVOS SIZE: %i\n\n",list_size(lista_archivos));
 		un_archivo = list_get(lista_archivos,j);
-		printf("nodos control 4\n");
+		vectorDeBitArrays = malloc(sizeof(t_cargaBitarray_aux)*list_size(un_archivo->bloques));
+		//printf("nodos control 4\n");
 		for(h=0;h < un_archivo->cantidadDeBloques;h++){
-			t_bloque* bloque = list_get(un_archivo->bloques,h);
-			printf("ID DEL BLOQUE DENTRO DEL BITARRAY: %i\n",bloque->NumeroBloque);
-			vectorDeBitArrays = realloc(vectorDeBitArrays,sizeof(t_cargaBitarray_aux)*((*sub_indice)+1));
+			bloque = list_get(un_archivo->bloques,h);
+			//printf("ID DEL BLOQUE DENTRO DEL BITARRAY: %i\n",bloque->NumeroBloque);
+			//cargo t_bitarray_aux
+			//vectorDeBitArrays = malloc(sizeof(t_cargaBitarray_aux)*((*sub_indice)+1));
 			vectorDeBitArrays[(*sub_indice)].nombre_arch = un_archivo->nombre;
+			//printf("NOMBRE DEL ARCHIVO: %s \n", vectorDeBitArrays[(*sub_indice)].nombre_arch);
 			vectorDeBitArrays[(*sub_indice)].bloque_arch = bloque->NumeroBloque;
+			//printf("ID DEL BLOQUE DENTRO DEL BITARRAY: %i\n\n", vectorDeBitArrays[(*sub_indice)].bloque_arch);
+			//printf("CANTIDAD DE NODOS: %i\n",cantidadDeNodos);
+			un_nombre = malloc(cantidadDeNodos*sizeof(char));
+			//printf("TAMAÑO DEL CHAR*: %i\n",cantidadDeNodos*sizeof(char));
 			vectorDeBitArrays[(*sub_indice)].bitmap = bitarray_create(un_nombre, cantidadDeNodos);
+			printf("TAMAÑO DEL BITMAP ADENTRO: %i\n",vectorDeBitArrays[(*sub_indice)].bitmap->size);
 			inicializarBitarray(vectorDeBitArrays[(*sub_indice)].bitmap, cantidadDeNodos);
-				for(k=0;k < 3; k++){
-					int pos;
-				//	if(buscarPorNodo(list_get(bloque->)->idNodo, nodos_activos, pos)) bitarray_set_bit(vectorDeBitArrays[(*sub_indice)].bitmap, pos);
-				}
+			//Cargo el bitarray
+			nodo_id = bloque->copia1_idnodo;
+			pos = get_nodo_pos(nodos_activos, nodo_id);
+			//printf("POSICION: %i\n",pos);
+			bitarray_set_bit(vectorDeBitArrays[(*sub_indice)].bitmap, pos);
+
+			nodo_id = bloque->copia2_idnodo;
+			pos = get_nodo_pos(nodos_activos, nodo_id);
+			//printf("POSICION: %i\n",pos);
+			bitarray_set_bit(vectorDeBitArrays[(*sub_indice)].bitmap, pos);
+
+			nodo_id = bloque->copia3_idnodo;
+			pos = get_nodo_pos(nodos_activos, nodo_id);
+			//printf("POSICION: %i\n",pos);
+			bitarray_set_bit(vectorDeBitArrays[(*sub_indice)].bitmap, pos);
+
 			(*sub_indice) ++;
+
+			/*
+			printf("---------------------BITMAP-----------------------\n");
+			for(k=0;k<cant_nodos;k++){
+				if(bitarray_test_bit(vectorDeBitArrays[(*sub_indice)].bitmap, k)) printf("    1    |");
+				else printf("    0    |");
+			}
+			printf("\n");
+			*/
+/*
 		}
+		printf("TAMAÑO DEL BITMAP: %i\n",vectorDeBitArrays[2].bitmap->size);
+		for(k=0;k<=cantidadDeNodos;k++){
+			if(bitarray_test_bit(vectorDeBitArrays[2].bitmap, k) == true) printf("BIT DE TESTEO N° %i: 1\n",k);
+			else printf("BIT DE TESTEO N° %i: 0\n",k);
+		}
+		printf("dsadjsadsakj\n");
+		printf("TAMAÑO DEL BITMAP %i\n",bitarray_get_max_bit(vectorDeBitArrays[2].bitmap));
 	}
-	free(un_nombre);
+	//free(un_nombre);
 	printf("nodos control 5\n");
 	return vectorDeBitArrays; //LIBERAR ESTA MEMORIA DESP Y ELIMINAR CADA BITARRAY TMB
 }
-
+*/
+/*
 void buscarVictimasPorBloque(t_cargaBitarray_aux bitmap[], int tamanio, int *vector_contador, int cant_nodos, int *bloqueArch_pos, int *nodo){
 	int min = bitarray_get_max_bit(bitmap[0].bitmap);
 	int min_pos = 0;
@@ -118,8 +165,41 @@ void cargar_nodos_en_array(int *nodos){
 		nodos[i] = nodo->id_nodo;
 	}
 }
+*/
+int buscarPorContadores(t_bloque *bloque){
+	int i;
+	int min = contadores_nodos[0];
+	int pos_id_nodo;
+	int id_nodo = 555;
+	bool flag = false;
+	t_nodo *data_nodo = malloc(sizeof(t_nodo));
+	do
+	{
+	//---------------------------------------BUSCO EL MAS CHICO DE LOS CONTADORES--------------------------------
+		for(i=1;i<cant_nodos;i++){
+			if(contadores_nodos[i] < min){
+				min = contadores_nodos[i];
+				pos_id_nodo = i;
+			}
+		}
+		data_nodo = list_get(lista_nodos_estado,pos_id_nodo);
+	//-------------------------------------BUSCO SI LA COPIA ESTA EN EL NODO MAS CHICO---------------------------
+		for(i=0;i<3;i++){
+			if(data_nodo->id_nodo == bloque->copias[i].idNodo){
+				id_nodo = data_nodo->id_nodo;
+				flag = true;
+				break;
+			}
+		}
+		if(flag == false) contadores_nodos[pos_id_nodo] = 4444;
 
-void planificarMap(){
+	} while(flag == false);
+
+	return id_nodo;
+}
+
+void planificarMap(int id_job){
+	/*
 	// PARA PLANIFICAR NECESITO SABER LOS NODOS ACTIVOS. PARA ESO SE LO PIDO AL FS
 	int cantidad_nodos_activos = list_size(lista_nodos_estado);	// ESTO ME LO MANDA EL FS, JUNTO CON LOS NODOS_ACTIVOS
 	int *nodos_activos = malloc(sizeof(int)*cantidad_nodos_activos);
@@ -177,5 +257,36 @@ void planificarMap(){
     free(vector_contador);
     */
 
-}
+    //---------------------OBTENGO LA INFO DEL JOB--------------------------------------
+    t_infoJob *job = malloc(sizeof(t_infoJob));
+    job = list_get(lista_jobs,id_job);
+    //----------------------------------------------------------------------------------
 
+    t_job_procesos *tabla_procesos_job = malloc(sizeof(t_job_procesos));
+    tabla_procesos_job = list_get(lista_tabla_procesos,id_job);
+    t_tablaProcesos_porJob *tabla_procesos;
+    int j,i;
+    int id_nodo = 0;
+    t_archivo *un_archivo = malloc(sizeof(t_archivo));
+    t_bloque *un_bloque = malloc(sizeof(t_bloque));
+
+    printf("       es        |        id      |        bq       |        nm       |\n");
+    for(i=0;i<list_size(job->archivos_job);i++){
+    	un_archivo = list_get(lista_archivos,i);
+    	for(j=0;j<list_size(un_archivo->bloques);j++){
+    		un_bloque = list_get(un_archivo->bloques,j);
+    		id_nodo = buscarPorContadores(un_bloque);
+    		tabla_procesos = malloc(sizeof(t_tablaProcesos_porJob));
+    		tabla_procesos->estado = 0; //Estado 0 = espera por enviarse
+    		tabla_procesos->id_nodo = id_nodo;
+    		tabla_procesos->bloque_archivo = un_bloque->NumeroBloque;
+    		tabla_procesos->nombre_archivo = un_archivo->nombre;
+    		list_add(tabla_procesos_job->tabla_procesos,tabla_procesos);
+    		printf("       %i        |        %i      |        %i       |        %s       |\n",tabla_procesos->estado,tabla_procesos->id_nodo,tabla_procesos->bloque_archivo,tabla_procesos->nombre_archivo);
+
+    	}
+    }
+
+    free(un_archivo);
+    free(un_bloque);
+}
